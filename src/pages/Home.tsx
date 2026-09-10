ac73670a91743e87da3caf18e39a31c41ab7ba50
Benson848 <db860770@gmail.com>
2026-09-10 20:49:49 +0100

    Add README

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -47,8 +47,8 @@ export default function Home() {
       <main>
         <p className="eyebrow">FOUNDATION TRACK</p>
         <h1>
-          Aliyu Walid Abubakar <br />
-          2024/1/95125CP <br />
+          Benson Nnamdi David <br />
+          2024/1/99087CM <br />
           Telecommunications Engineering
         </h1>
         <p className="lede">Save a note, refresh the page, then test the same experience with the network turned off.</p>
